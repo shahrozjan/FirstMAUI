8da3bc5f4c8ab3b33c27b2b995b161de174b10c3
shahrozjan <51943448+shahrozjan@users.noreply.github.com>
2023-05-28 17:10:22 -0400

    First Pull (#1)
    * first blank commit
    
    * PhoneWord Translator
    
    * some extra phone dialer changes

---
+++ FirstMAUI/FirstMAUI/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿namespace FirstMAUI;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+    string translatedNumber;
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        translatedNumber = FirstMAUI.PhonewordTranslator.ToNumber(enteredNumber);
+
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            CallButton.IsEnabled = true;
+            CallButton.Text = "Call " + translatedNumber;
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
+    }
+
+    async void OnCall(object sender, System.EventArgs e)
+    {
+        if (await this.DisplayAlert(
+            "Dial a Number",
+            "Would you like to call " + translatedNumber + "?",
+            "Yes",
+            "No"))
+        {
+            try
+            {
+                if (PhoneDialer.IsSupported)
+                    PhoneDialer.Open(translatedNumber);
+                PhoneDialer.Open(translatedNumber);
+                
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+            }
+            catch (Exception)
+            {
+                // Other error has occurred.
+                await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+            }
+        }
+    }
+
+}
+
+
